1a0cf5f1319156fad9ab0f97cc76a3f939240064
Marce Legue <marcelegue@gmail.com>
2019-05-26 04:03:29 +0000

    CELE_darwin_mapping.sh edited online with Bitbucket

--- CELE_darwin_mapping.sh
+++ CELE_darwin_mapping.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=CELE_mapping 
+#SBATCH --mem-per-cpu=5G
+#SBATCH -n 6
+#SBATCH -c 2
+#SBATCH --output=CELE_output.txt
+#SBATCH --error=CELE_error.txt
+
+module load Bowtie2
+
 bowtie2-build -f c_elegans.PRJNA13758.WS267.genomic.fa CELE_F1F6
+
+for file in trimmed*
+do bowtie2 -q --phred33 -N 0 -L 17 -i S,0,2.50 -x CELE_F1F6 -U $file -S CELE_mapped_$file
+done
+
