baca337500b2cbcdd378f31ed195feb46adaec0e
Marce Legue <marcelegue@gmail.com>
2019-05-27 02:50:26 +0000

    CELE_bam_sort_.sh edited online with Bitbucket

--- CELE_sort_merge.sh
+++ CELE_sort_merge.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=CELE_merge
+#SBATCH --job-name=CELE_bam_sort
 #SBATCH --mem-per-cpu=5G
 #SBATCH -n 6
 #SBATCH -c 2
-#SBATCH --output=CELE_merge_output.txt
-#SBATCH --error=CELE_merge_error.txt
+#SBATCH --output=CELE_bam_output.txt
+#SBATCH --error=CELE_bam_error.txt
 
 module load SAMtools
 
 for file in CELE_mapped*
 do 
-samtools view -bS $file | samtools sort - sorted_$file.bam
+samtools view -bS $file > $file.bam
+done
+for bam in *.bam
+do
+samtools sort -o sorted_$file.bam
 done
