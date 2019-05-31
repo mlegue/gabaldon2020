81e46206a9a6cceb0bbd58b3c0ed16309dd91a46
Marce Legue <marcelegue@gmail.com>
2019-05-31 18:53:57 +0000

    CELE_darwin_mapping.sh edited online with Bitbucket

--- CELE_darwin_mapping.sh
+++ CELE_darwin_mapping.sh
@@ -11,6 +11,6 @@ module load Bowtie2
 bowtie2-build -f c_elegans.PRJNA13758.WS267.genomic.fa CELE_F1F6
 
 for file in trimmed*
-do bowtie2 -q --phred33 -N 0 -L 17 -i S,0,2.50 -x CELE_F1F6 -U $file -S CELE_mapped_$file
+do bowtie2 -q --phred33 -N 0 -L 17 -i S,0,2.50 -x CELE_F1F6 -U $file -S CELE_mapped_$file.sam
 done
 
