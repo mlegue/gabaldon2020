60046064683fd6b1fca2d37a5347ba86243124f0
Marce Legue <marcelegue@gmail.com>
2019-06-03 18:57:07 +0000

    Rsubread_F1F6.r edited online with Bitbucket

--- Rsubread_F1F6.r
+++ Rsubread_F1F6.r
@@ -1,4 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=CELE_Rsubread
+#SBATCH --mem-per-cpu=5G
+#SBATCH -n 6
+#SBATCH -c 2
+#SBATCH --output=CELE_Rsubread_output.txt
+#SBATCH --error=CELE_Rsubread_error.txt
 
+module load R/3.4.0
 
 library(Rsubread)
 
@@ -7,8 +15,7 @@ fastq.trim.files <- list.files(path = "./Cutadapted", pattern = ".fastq.gz$", fu
 origin <- fastq.trim.files
 
 # build an small-RNA index using ensembl c_elegans.PRJNA13758.WS267.genomic.fa
-
-
+mkdir indexR 
 buildindex(basename="./reference_index",reference=ref)
 
 # align the sample read data provided in this packge to the sample reference
